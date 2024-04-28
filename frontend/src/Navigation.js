60373a8cb3dd277de05b0e213feda85169affde0
Josh Rivera <beps@Mactop.local>
2024-04-28 11:14:22 -0500

    Added logout button to make removing token easier

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -6,7 +6,13 @@ function Navigation() {
 
     const history = useHistory()
 
-    const { currentUser } = useContext(CurrentUser)
+    const { currentUser, setCurrentUser } = useContext(CurrentUser)
+
+    const logOut = () => {
+        localStorage.removeItem('token')
+        setCurrentUser(null)
+        history.push('/')
+    }
 
     let loginActions = (
         <>
@@ -25,9 +31,14 @@ function Navigation() {
 
     if (currentUser) {
         loginActions = (
+            <>
             <li style={{ float: 'right' }}>
                 Logged in as {currentUser.firstName} {currentUser.lastName}
             </li>
+            <li style={{ float: 'right' }}>
+                <button onClick={logOut}>Log out</button>
+            </li>
+            </>
         )
     }
 
