d8ee8febd81b051145be072487d6c799268b6b68
Josh Rivera <beps@Mactop.local>
2024-04-28 13:45:35 -0500

    Made it so that edit and delete buttons for places are only visible to admin

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -105,6 +105,20 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+	if(currentUser?.role === 'admin'){
+		placeActions = (
+			<>
+				<a className="btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>
+				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
+
 	return (
 		<main>
 			<div className="row">
@@ -131,12 +145,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
